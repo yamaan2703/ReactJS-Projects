56e280b30cb78d0bd8c65185b3c8a5d84656a338
yamaan2703 <115937602+yamaan2703@users.noreply.github.com>
2024-03-04 05:27:19 -0800

    main

--- e-commerce-app/src/Screens/CartPage.js
+++ e-commerce-app/src/Screens/CartPage.js
@@ -1,38 +1,27 @@
-import WebBtn from "../Components/WebBtn";
-import MenuImg from "../images/Menu.jpg";
+import React from 'react';
+import bg1 from '../images/bg2.jpg';
 
 export default function CartPage() {
   return (
-    <>
-      <div className="bg-[#0d1819] ">
-      <div className="menu_img relative h-[500px] bg-center bg-cover">
-                <div className="menu_overlay absolute inset-0 bg-black opacity-50"></div>
-                <img src={MenuImg} alt="Menu" className="w-full h-full object-cover" />
-                <h1 className="text-white text-5xl md:text-3xl lg:text-5xl font-serif text-center  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">Our restaurant offers a delightful menu with exceptional flavors at reasonable prices...</h1>
+    <div className="bg-[#0d1819] relative">
+      <div className="container mx-auto pt-40">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="cart bg-sky-500 p-5 rounded-lg flex items-center">
+            <img src={bg1} alt="Product" className="w-48 h-auto rounded-lg mr-4" />
+            <div>
+              <h1 className="text-white text-xl font-bold">Zinger Burger</h1>
+              <p className="text-white">$ 199</p>
+              <p className="text-white">*5.7</p>
+              <p className="text-white">Coffee's flavor hinges on factors like bean type, origin, roast, grind size, brewing method, water quality, and altitude. Each element adds a distinct note to the brew, creating a rich tapestry of tastes and aromas for aficionados to savor.</p>
             </div>
-
-            
-        <div className="container">
-          <div className="card bg-gray-700 mx-3 md:mx-1 lg:mx-5 text-white text-center flex-column justify-start items-center rounded-xl relative shadow-md my-2 shadow-[#e4c590]">
-            <div className="absolute top-0 right-0 p-2 bg-black bg-opacity-50 rounded-tr-xl rounded-bl-xl">
-              <p className="text-yellow-400">4.5</p>
-            </div>
-            <div className="card-body">
-              <h5 className="card-title text-xl text-[#e4c590] font-serif">Cup Cake</h5>
-              <p className="card-text text-xl text-yellow-600 flex justify-center">$<span className="mx-1"> 199</span></p>
-            </div>
-            <div className="my-1 mb-3">
-              <WebBtn label="Order Now" />
-            </div>
+          </div>
+          <div className="bill bg-yellow-500 p-5 rounded-lg">
+            <p className="text-white">Total Quantity</p>
+            <p className="text-white">Total Price</p>
+            <button className="bg-blue-500 text-white px-4 py-2 rounded-md">Button</button>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
-
-
-
-
-
-                
