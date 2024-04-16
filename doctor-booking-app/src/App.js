9709fcbfe42b6fc52c95eb78425b98e9409a5d5d
yamaan2703 <115937602+yamaan2703@users.noreply.github.com>
2024-04-16 04:33:36 -0700

    main

---
+++ doctor-booking-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1 className='text-center text-5xl'>Docto Booking App</h1>
+    </>
+  );
+}
+
+export default App;
